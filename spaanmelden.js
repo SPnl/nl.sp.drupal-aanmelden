7b46b40a7d7258d1afd0c73942b343148fbf7929
Kevin Levie <kevin@levity.nl>
2014-11-19 11:05:03 +0100

    Pre-accept update

--- spaanmelden.js
+++ spaanmelden.js
@@ -4,11 +4,24 @@
 
     Drupal.ajax.prototype.commands.scrollToTop = function (ajax, response, status) {
         var formOffset = $('#spaanmelden_form').offset();
-        $('html, body').animate({ scrollTop: formOffset.top }, 400, 'easeOutQuad');
+        $('html, body').animate({scrollTop: formOffset.top}, 400, 'easeOutQuad');
     };
 
     Drupal.ajax.prototype.commands.scrollToWrapper = function (ajax, response, status) {
-        $('html, body').animate({ scrollTop: $(ajax.wrapper, ajax.form).offset().top - 100 }, 400, 'easeOutQuad');
+        $('html, body').animate({scrollTop: $(ajax.wrapper, ajax.form).offset().top - 100}, 400, 'easeOutQuad');
     };
+
+    Drupal.ajax.prototype.commands.focusElement = function (ajax, response, status) {
+        if (response && response.element) {
+            setTimeout(function () {
+                var next = $('.' + response.element).next('.form-item');
+                if (next) {
+                    var field = next.find('input, select').first();
+                    if (field)
+                        field.focus();
+                }
+            }, 100);
+        }
+    }
 
 }(jQuery));
